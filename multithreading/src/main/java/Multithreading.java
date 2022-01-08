91ea61d5fa9b8a8cf168d790818c101b778d4e96
Loilei <marcelina.gasiorek@gmail.com>
2022-01-08 11:33:14 +0100

    Threads and counter implemented

--- multithreading/src/main/java/Multithreading.java
+++ multithreading/src/main/java/Multithreading.java
@@ -1,10 +1,12 @@
 public class Multithreading {
+    static int counter = 0;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        int counter = 0;
-
-        Runnable runnable = () -> counter++;
+        Runnable runnable = () -> {
+            for (int i = 0; i < 100000; i++)
+                counter++;
+        };
 
         Thread thread1 = new Thread(runnable);
         Thread thread2 = new Thread(runnable);
@@ -13,6 +15,13 @@ public class Multithreading {
         thread1.start();
         thread2.start();
         thread3.start();
+
+        thread1.join();
+        thread2.join();
+        thread3.join();
+
+        System.out.println(counter);
+        System.out.println(counter);
 
     }
 }
